1186b185ce5df8c360888b27b287f6cc70380796
Tsubasa Kubo <nepia.infinity12335@gmail.com>
2023-11-11 19:57:02 +0900

    show()の記述を追加して、グラフが表示されるようにした。

--- weight.py
+++ weight.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
 # 12ヶ月分の体重データ（単位：kg）
-weights = [68, 67.5, 67, 66.5, 66, 65.5, 65, 64.5, 64, 63.5, 63, 62.5]
+weights = [68, 67.5, 67, 70, 72, 69, 71, 70.5, 69, 67, 68.5, 67]
 plt.plot(weights)
+plt.show()
